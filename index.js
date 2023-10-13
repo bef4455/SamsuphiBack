9361d2cb8fc4944b23a872005c4ff0cc0c8c824b
bef4 <befacool@gmail.com>
2023-10-13 16:53:03 +0200

    change to Sam

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello code");
+    res.send("Hello Samsuphi");
 });
 
 app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+    console.log("Server is listening on port 3000");
 });
